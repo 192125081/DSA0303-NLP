e021595df2cb4824c8fd594cae0699291da824c1
192125081 <113340579+192125081@users.noreply.github.com>
2023-10-10 10:00:22 +0530

    Add files via upload

---
+++ 1.parts of speech.py
@@ -0,0 +1,15 @@
+import nltk
+
+# Download necessary resources
+
+def get_parts_of_speech(sentence):
+    tokens = nltk.word_tokenize(sentence)
+    pos_tags = nltk.pos_tag(tokens)
+    return pos_tags
+
+# Example usage
+sentence = "The quick brown fox jumps over the lazy dog"
+parts_of_speech = get_parts_of_speech(sentence)
+
+for word, pos in parts_of_speech:
+    print(f"{word}: {pos}")
